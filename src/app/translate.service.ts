dc6cd3fcc03eaf63f7b196268651e0cee113d31f
Ying-Wang1994 <wywyflash@gmail.com>
2023-02-12 14:48:54 +1100

    yaml

--- src/app/translate.service.ts
+++ src/app/translate.service.ts
@@ -40,11 +40,7 @@ export class TranslateService {
     private httpClient: HttpClient
   ) { }
 
-  /**
-   * @desc 调用谷歌翻译，需要 content -> 要翻译的内容，和 target -> 目标语言两个参数
-   * @param content
-   * @param target
-   */
+  
   translate(content: string, target: Language): Observable<string> {
     return this.httpClient.post<GoogleTranslationResult>(`https://translation.googleapis.com/language/translate/v2?key=AIzaSyA9PbwV3vJsHydx7olAXxrqAH2aaz7LggE`, {
       q: content,
